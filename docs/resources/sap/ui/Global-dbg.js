254675afbe6a3f6be3b4eeb19ce6c0203e2dc596
IObert <IObert@users.noreply.github.com>
2021-10-08 10:48:06 +0000

    Chore: Build webapp

--- docs/resources/sap/ui/Global-dbg.js
+++ docs/resources/sap/ui/Global-dbg.js
@@ -76,7 +76,7 @@ sap.ui.define([
 		 * @type string
 		 */
 		version: "1.76.0",
-		buildinfo : { lastchange : "${lastchange}", buildtime : "${buildtime}" }
+		buildinfo : { lastchange : "${lastchange}", buildtime : "20211008-1046" }
 	});
 
 	var oCfgData = window["sap-ui-config"] || {};
