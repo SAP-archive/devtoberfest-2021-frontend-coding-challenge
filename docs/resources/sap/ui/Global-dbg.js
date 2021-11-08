a1aa48285d928687ca3e9c867cd65104671a8fbe
Nils Lutz <nils.lutz@btc-ag.com>
2021-11-08 21:08:29 +0100

    build: update build

--- docs/resources/sap/ui/Global-dbg.js
+++ docs/resources/sap/ui/Global-dbg.js
@@ -76,7 +76,7 @@ sap.ui.define([
 		 * @type string
 		 */
 		version: "1.95.0",
-		buildinfo : { lastchange : "${lastchange}", buildtime : "20211108-2034" }
+		buildinfo : { lastchange : "${lastchange}", buildtime : "20211108-2105" }
 	});
 
 	var oCfgData = window["sap-ui-config"] || {};
