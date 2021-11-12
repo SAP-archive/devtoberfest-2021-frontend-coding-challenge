07e1dc657a37136ad56ac1409b3f49f5ec5ab534
Ibrahim Hamlaoui <ibrahim.hamlaoui@sap.com>
2021-11-11 23:48:27 -0500

    changes

--- docs/resources/sap/m/changeHandler/SelectIconTabBarFilter.js
+++ docs/resources/sap/m/changeHandler/SelectIconTabBarFilter.js
@@ -3,4 +3,4 @@
  * (c) Copyright 2009-2021 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
-sap.ui.define(["sap/ui/fl/library"],function(e){"use strict";var t={};t.applyChange=function(e,t,n){var r=n.modifier;var i=e.getDefinition();r.setProperty(t,"selectedKey",i.content.selectedKey);e.setRevertData(i.content.previousSelectedKey)};t.revertChange=function(e,t,n){var r=n.modifier;var i=e.getRevertData();r.setProperty(t,"selectedKey",i);e.resetRevertData()};t.completeChangeContent=function(e,t,n){};t.getCondenserInfo=function(t){return{affectedControl:t.getSelector(),classification:e.condenser.Classification.LastOneWins,uniqueKey:t.getContent().selectedKey}};return t});
+sap.ui.define(function(){"use strict";var e={};e.applyChange=function(e,t,r){var n=r.modifier;var a=e.getDefinition();var i=a.content;return n.getProperty(t,"selectedKey").then(function(r){var a={selectedKey:r};n.setProperty(t,"selectedKey",i);e.setRevertData(a)})};e.revertChange=function(e,t,r){var n=r.modifier;var a=e.getRevertData();var i=a.selectedKey;n.setProperty(t,"selectedKey",i)};e.completeChangeContent=function(e,t,r){};return e});
